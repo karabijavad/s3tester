e0044f661e00e8adedbd79e5a91065901dbbde89
Javad Karabi <karabijavad@gmail.com>
2018-11-25 23:19:03 -0600

    wip

--- terraform/s3encryptiontester.tf
+++ terraform/s3encryptiontester.tf
@@ -1,4 +1,56 @@
 resource "random_pet" "echo" {}
+
+resource "aws_cloudtrail" "echo_cloudtrailer" {
+  name           = "s3tester-echo-cloudtrailer-${random_pet.echo.id}"
+  s3_bucket_name = "${aws_s3_bucket.echo_cloudtrailer.id}"
+  include_global_service_events = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["${aws_s3_bucket.echo.arn}/"]
+    }
+  }
+}
+
+resource "aws_s3_bucket" "echo_cloudtrailer" {
+  bucket        = "s3tester-echo-cloudtrailer-${random_pet.echo.id}"
+}
+
+resource "aws_s3_bucket_policy" "echo_cloudtrailer" {
+  bucket = "${aws_s3_bucket.echo_cloudtrailer.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {"Service": "cloudtrail.amazonaws.com"},
+            "Action": "s3:GetBucketAcl",
+            "Resource": "${aws_s3_bucket.echo_cloudtrailer.arn}"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {"Service": "cloudtrail.amazonaws.com"},
+            "Action": "s3:PutObject",
+            "Resource": "${aws_s3_bucket.echo_cloudtrailer.arn}/*",
+            "Condition": {"StringEquals": {"s3:x-amz-acl": "bucket-owner-full-control"}}
+        }
+    ]
+}
+POLICY
+}
+
+resource "local_file" "echo_cloudtrailer_arn" {
+  content = "${aws_s3_bucket.echo_cloudtrailer.arn}"
+
+  filename = "output/echo_cloudtrailer_arn"
+}
+
 resource "aws_kms_key" "echo" {
   deletion_window_in_days = 7
 }
