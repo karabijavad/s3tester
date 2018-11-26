c9f06460b7223f3caee7fd80a6356ca80a6e5a73
Javad Karabi <karabijavad@gmail.com>
2018-11-25 18:24:18 -0600

    wip

--- s3tester/s3tester.py
+++ s3tester/s3tester.py
@@ -14,10 +14,14 @@ bravo_key_secret = open("../terraform/output/bravo_key_secret").read()
 charlie_key_id = open("../terraform/output/charlie_key_id").read()
 charlie_key_secret = open("../terraform/output/charlie_key_secret").read()
 
+echo_key_id = open("../terraform/output/echo_key_id").read()
+echo_key_secret = open("../terraform/output/echo_key_secret").read()
+
 delta_role_arn = open("../terraform/output/delta_role_arn").read()
 
 alpha_bucket_name = open("../terraform/output/alpha_bucket_name").read()
 bravo_bucket_name = open("../terraform/output/bravo_bucket_name").read()
+echo_bucket_name = open("../terraform/output/echo_bucket_name").read()
 
 
 class TestAlpha(unittest.TestCase):
@@ -34,8 +38,10 @@ class TestAlpha(unittest.TestCase):
             [_ for _ in bravo_bucket.objects.all()]
 
     def test_cant_assume_delta(self):
-        client = boto3.client('sts', aws_access_key_id=alpha_key_id,
-                              aws_secret_access_key=alpha_key_secret)
+        client = boto3.client(
+            'sts',
+            aws_access_key_id=alpha_key_id,
+            aws_secret_access_key=alpha_key_secret)
         with self.assertRaises(ClientError):
             client.assume_role(
                 RoleArn=delta_role_arn,
@@ -57,16 +63,17 @@ class TestBravo(unittest.TestCase):
             [_ for _ in alpha_bucket.objects.all()]
 
     def test_can_assume_delta_to_see_alphas_objects(self):
-        sts_client = boto3.client('sts', aws_access_key_id=bravo_key_id,
-                                  aws_secret_access_key=bravo_key_secret)
-        role_info = sts_client.assume_role(RoleArn=delta_role_arn,
-                                           RoleSessionName="test")
+        sts_client = boto3.client(
+            'sts',
+            aws_access_key_id=bravo_key_id,
+            aws_secret_access_key=bravo_key_secret)
+        role_info = sts_client.assume_role(
+            RoleArn=delta_role_arn, RoleSessionName="test")
         assumed_alpha_s3_client = boto3.resource(
             's3',
             aws_access_key_id=role_info['Credentials']['AccessKeyId'],
             aws_secret_access_key=role_info['Credentials']['SecretAccessKey'],
-            aws_session_token=role_info['Credentials']['SessionToken']
-        )
+            aws_session_token=role_info['Credentials']['SessionToken'])
         alpha_bucket = assumed_alpha_s3_client.Bucket(name=alpha_bucket_name)
         [_ for _ in alpha_bucket.objects.all()]
 
@@ -88,13 +95,20 @@ class TestCharlie(unittest.TestCase):
         [_ for _ in bravo_bucket.objects.all()]
 
     def test_cant_assume_delta(self):
-        client = boto3.client('sts', aws_access_key_id=alpha_key_id,
-                              aws_secret_access_key=alpha_key_secret)
+        client = boto3.client(
+            'sts',
+            aws_access_key_id=alpha_key_id,
+            aws_secret_access_key=alpha_key_secret)
         with self.assertRaises(ClientError):
             client.assume_role(
                 RoleArn=delta_role_arn,
                 RoleSessionName="test",
             )
+
+
+class TestEncryption(unittest.TestCase):
+    def test_download_unencrypted(self):
+        self.assertTrue(False)
 
 
 if __name__ == '__main__':
